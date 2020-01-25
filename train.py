bed97b33a7f469f4df046620340a327dce26198e
mr-robot0 <vptl185@gmail.com>
2020-01-26 00:15:43 +0530

    training from generator

--- train.py
+++ train.py
@@ -34,11 +34,6 @@ RESIZE = True if "--resize" in sys.argv else False
 SHAPE = (137,236)
 TRAIN_SHAPE = (-1,137,236,1)
 
-OPTIMIZER = sys.argv[sys.argv.index("-opt")+1] if "-opt" in sys.argv else "adam"
-ARCHITECTURE = sys.argv[sys.argv.index("-arch")+1] if "-arch" in sys.argv else "vgg16"
-
-
-
 class LabelEncoder():
     def __init__(self,):
         self._classes = []
@@ -49,7 +44,7 @@ class LabelEncoder():
     def transform(self,labels):
         return np.fromiter(map(self._classes.index,labels),dtype=np.int)
 
-def create_vgg16(input_shape,output_shape):
+def create_model(input_shape):
     model = Sequential()
 
     ### Block 1
@@ -82,8 +77,6 @@ def create_vgg16(input_shape,output_shape):
     model.add(Flatten())
     model.add(Dense(units=4096,activation="relu"))
     model.add(Dense(units=4096,activation="relu"))
-    model.add(Dense(units=output_shape, activation="softmax"))
-
     return model
 
 def save_score(y,y_pred):
@@ -95,41 +88,31 @@ def save_score(y,y_pred):
     print (scores)
     json.dump(open("./score.json","w+"),scores)
 
-def load_data(file_id):
-    images = pd.read_parquet(f"./train/train_image_data_{file_id}.parquet")
-    _labels = labels.loc[images.pop("image_id")]['grapheme_root']
 
-    return images,_labels    
-
-labels = pd.read_csv("./train.csv")
-labels.index = labels.image_id
-
-le_grapheme_root = LabelEncoder()
-le_grapheme_root.fit(labels.grapheme_root)
-labels['grapheme_root'] = le_grapheme_root.transform(labels.grapheme_root)
-
-
+train_df = pd.read_csv("./train.csv")
+train_df.index = train_df.image_id.values
+train_grapheme = pd.get_dummies(train_df['grapheme_root'])
 
 EPOCHS = 20
 BATCH_SIZE = 5
 
-model = create_vgg16(input_shape=(137,236,1),output_shape=len(list(set(labels.grapheme_root))))
-model.compile(optimizer=OPTIMIZER,loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+model = create_model(input_shape=(137,236,1))
+model.add(Dense(units=168, activation="softmax"))
+model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=["accuracy"])
 
 for epoch in range(EPOCHS):
     for file_id in range(4):
-        X,Y = load_data(file_id)
+        X = pd.read_parquet(f"./train_image_data_{file_id}.parquet")
+        Y = train_grapheme.loc[X.pop("image_id")]
+
         for index in tqdm(range(BATCH_SIZE,X.shape[0],BATCH_SIZE)):
             X_ = X[index - BATCH_SIZE:index].values.reshape(TRAIN_SHAPE)
             Y_ = Y[index - BATCH_SIZE:index]
             model.fit(X_,Y_,batch_size=BATCH_SIZE,verbose=False)
 
-        X_ = X[index:]
+        X_ = X[index:].values.reshape(TRAIN_SHAPE)
         Y_ = Y[index:]
         model.fit(X_,Y_)
-
-# model.evaluate(x,y)
-# save_score(model,x,y)
 
 notify.success()
 
